f072c483857d3c79c5829d3ce7d0e8c42fbfc516
Juliano Castilho <julianocomg@gmail.com>
2016-02-15 19:22:57 -0300

    Adding support for field name

--- android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
+++ android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
@@ -109,11 +109,12 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
                 ReadableMap file = files.getMap(i);
                 String filename = file.getString("filename");
                 String filepath = file.getString("filepath");
+                String fieldName = file.getString("name");
                 filepath = filepath.replace("file://", "");
                 fileInputStream = new FileInputStream(filepath);
 
                 outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\"" + filename + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + fieldName + "\";filename=\"" + filename + "\"" + lineEnd);
                 outputStream.writeBytes(lineEnd);
 
                 bytesAvailable = fileInputStream.available();
